4d594dfdffc86f3dde08d9e0f3f34ae782aee5fc
arthur <arthurfelipe5567@gmail.com>
2024-09-16 13:45:57 -0300

    refactor: add tailwind to activity component

--- src/components/Activity.tsx
+++ src/components/Activity.tsx
@@ -9,8 +9,8 @@ export default function Activity({
 }>) {
   const {name, subject, endDate, isActive} = activity
   return (
-    <li onClick={() => onClick(activity)}>
-      <div>
+    <li onClick={() => onClick(activity)} className="w-full flex flex-col justify-center p-6 border border-solid border-[#2f2f2f] rounded cursor-pointer">
+      <div className="w-full flex flex-col flex-wrap">
         <span><b>Atividade:</b> {name}</span>
         <span><b>Matéria:</b> {subject}</span>
         {isActive ? <span><b>Data de entrega:</b> {endDate}</span> : <span><b>Prazo de entrega encerrado!</b></span>}
