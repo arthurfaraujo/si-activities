f0bd0651883e9b1a5bc87d0e0cf292425563f50c
Arthur Araújo <arthurfelipe5567@gmail.com>
2024-09-30 07:42:02 -0300

    fix: add subject select do formData

--- src/components/Activity/ActivityForm.tsx
+++ src/components/Activity/ActivityForm.tsx
@@ -63,15 +63,18 @@ export default function ActivityForm() {
       })
 
       setIsSending(is => !is)
-      window.location.href = FRONT_RELATIVE_URL
+      // window.location.href = FRONT_RELATIVE_URL + "/home"
     } catch (e) {
       console.error('Error: ', e)
     }
   }
 
   return (
-    <form className='max-w-[400px] w-4/6 flex flex-wrap gap-4' onSubmit={handleSubmit}>
-      <label className='label-style'>
+    <form
+      className="max-w-[400px] w-4/6 flex flex-wrap gap-4"
+      onSubmit={handleSubmit}
+    >
+      <label className="label-style">
         <span>Nome</span>
         <input
           name="name"
@@ -79,29 +82,44 @@ export default function ActivityForm() {
           value={formData.name}
           onChange={handleChange}
           required
-          className='input-style'
+          className="input-style"
         />
       </label>
-      <label className='label-style'>
+      <label className="label-style">
         <span>Matéria</span>
-        <select className='input-style'>
+        <select
+          className="input-style"
+          name="subject_id"
+          onChange={e => {
+            const { name, value } = e.target
+
+            e.target.checkValidity()
+
+            setFormData(data => ({
+              ...data,
+              [name]: value
+            }))
+          }}
+        >
           <option value="-1">Nenhuma</option>
           {$subjects.map(subject => (
-            <option value={subject.id}>{subject.name}</option>
+            <option key={subject.id} value={subject.id}>
+              {subject.name}
+            </option>
           ))}
         </select>
       </label>
-      <label className='label-style'>
+      <label className="label-style">
         <span>Descrição</span>
         <input
           name="description"
           type="text"
           value={formData.description}
           onChange={handleChange}
-          className='input-style'
+          className="input-style"
         />
       </label>
-      <label className='label-style'>
+      <label className="label-style">
         <span>Data de criação</span>
         <input
           name="startDate"
@@ -109,10 +127,10 @@ export default function ActivityForm() {
           value={formData.startDate}
           onChange={handleChange}
           placeholder="dd/MM/aaaa"
-          className='input-style'
+          className="input-style"
         />
       </label>
-      <label className='label-style'>
+      <label className="label-style">
         <span>Data de entrega</span>
         <input
           name="endDate"
@@ -120,11 +138,20 @@ export default function ActivityForm() {
           value={formData.endDate}
           onChange={handleChange}
           placeholder="dd/MM/aaaa"
-          className='input-style'
+          className="input-style"
         />
       </label>
 
-      <button type="submit" className={'w-full text-lg p-2 border border-solid border-[#3a3a3a] rounded text-white bg-inherit cursor-pointer' + (isSending ? ' disabled-button' : "")} disabled={isSending}>Criar atividade</button>
+      <button
+        type="submit"
+        className={
+          'w-full text-lg p-2 border border-solid border-[#3a3a3a] rounded text-white bg-inherit cursor-pointer' +
+          (isSending ? ' disabled-button' : '')
+        }
+        disabled={isSending}
+      >
+        Criar atividade
+      </button>
     </form>
   )
 }
