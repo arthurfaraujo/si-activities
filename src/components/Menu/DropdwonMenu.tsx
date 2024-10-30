36fa17090308cfdb13d62f4f3812ef5b0e077efc
Arthur Araújo <arthurfelipe5567@gmail.com>
2024-10-29 21:57:27 -0300

    fix: change url route

--- src/components/Menu/DropdwonMenu.tsx
+++ src/components/Menu/DropdwonMenu.tsx
@@ -8,7 +8,7 @@ import { House, BookPlus, FilePlus, Star, ListPlus } from 'lucide-react'
 import { Button } from '../ui/button'
 import { DropdownMenuGroup } from '@radix-ui/react-dropdown-menu'
 
-export default function DropdwonMenu({url}: Readonly<{url: string}>) {
+export default function DropdwonMenu() {
   return (
     <div className='lg:hidden'>
       <DropdownMenu>
@@ -17,31 +17,31 @@ export default function DropdwonMenu({url}: Readonly<{url: string}>) {
         </DropdownMenuTrigger>
         <DropdownMenuContent className="border border-zinc-600 w-56 bg-zinc-900 text-inherit mt-1 mr-2">
           <DropdownMenuGroup>
-            <a href={url + '/home'}>
+            <a href={'/home'}>
               <DropdownMenuItem className="focus:bg-zinc-700 focus:text-inherit">
                 <House className="mr-2 h-4 w-4" />
                 <span>Início</span>
               </DropdownMenuItem>
             </a>
-            <a href={url + '/activity/create'}>
+            <a href={'/activity/create'}>
               <DropdownMenuItem className="focus:bg-zinc-700 focus:text-inherit">
                 <FilePlus className="mr-2 h-4 w-4" />
                 <span>Criar uma atividade</span>
               </DropdownMenuItem>
             </a>
-            <a href={url + '/subject/create'}>
+            <a href={'/subject/create'}>
               <DropdownMenuItem className="focus:bg-zinc-700 focus:text-inherit">
                 <BookPlus className="mr-2 h-4 w-4" />
                 <span>Criar uma matéria</span>
               </DropdownMenuItem>
             </a>
-            <a href={url + '/course/create'}>
+            <a href={'/course/create'}>
               <DropdownMenuItem className="focus:bg-zinc-700 focus:text-inherit">
                 <ListPlus className="mr-2 h-4 w-4" />
                 <span>Criar um curso</span>
               </DropdownMenuItem>
             </a>
-            <a href={url + '/rate'}>
+            <a href={'/rate'}>
               <DropdownMenuItem className="focus:bg-zinc-700 focus:text-inherit">
                 <Star className="mr-2 h-4 w-4" />
                 <span>Avalie o site</span>
--- src/components/Menu/ListMenu.tsx
+++ src/components/Menu/ListMenu.tsx
@@ -1,11 +1,11 @@
 import { BookPlus, FilePlus, House, Star, ListPlus } from 'lucide-react'
 import { Button } from '../ui/button'
 
-export default function ListMenu({url}: Readonly<{url: string}>) {
+export default function ListMenu() {
   return (
     <ul className='hidden lg:flex '>
       <li>
-        <a href={url + '/activity/create'}>
+        <a href={'/activity/create'}>
           <Button className="focus:bg-zinc-700 focus:text-inherit hover:bg-zinc-800 transition-all duration-200 flex gap-2">
             <FilePlus className="h-4 w-4" />
             <span>Atividade</span>
@@ -13,7 +13,7 @@ export default function ListMenu({url}: Readonly<{url: string}>) {
         </a>
       </li>
       <li>
-        <a href={url + '/subject/create'}>
+        <a href={'/subject/create'}>
           <Button className="focus:bg-zinc-700 focus:text-inherit hover:bg-zinc-800 transition-all duration-200 flex gap-2">
             <BookPlus className="h-4 w-4" />
             <span>Matéria</span>
@@ -21,7 +21,7 @@ export default function ListMenu({url}: Readonly<{url: string}>) {
         </a>
       </li>
       <li>
-        <a href={url + '/course/create'}>
+        <a href={'/course/create'}>
           <Button className="focus:bg-zinc-700 focus:text-inherit hover:bg-zinc-800 transition-all duration-200 flex gap-2">
             <ListPlus className="h-4 w-4" />
             <span>Curso</span>
@@ -29,7 +29,7 @@ export default function ListMenu({url}: Readonly<{url: string}>) {
         </a>
       </li>
       <li>
-        <a href={url + '/rate'}>
+        <a href={'/rate'}>
           <Button className="focus:bg-zinc-700 focus:text-inherit hover:bg-zinc-800 transition-all duration-200 flex gap-2">
             <Star className="h-4 w-4" />
             <span>Avalie o site</span>
@@ -37,7 +37,7 @@ export default function ListMenu({url}: Readonly<{url: string}>) {
         </a>
       </li>
       <li>
-        <a href={url + '/home'}>
+        <a href={'/home'}>
           <Button className="focus:bg-zinc-700 focus:text-inherit hover:bg-zinc-800 transition-all duration-200 flex gap-2">
             <House className="h-4 w-4" />
             <span>Início</span>
