79b28ad100f61332964722c7b93243b3de319dd6
Arthur Araújo <arthurfelipe5567@gmail.com>
2025-05-05 16:30:52 -0300

    feat: create subject api routes

---
+++ src/pages/api/subjects/index.ts
@@ -0,0 +1,29 @@
+export const prerender = false
+import type { APIRoute } from 'astro'
+
+const API_URL = process.env.API_URL
+
+export const POST: APIRoute = async ({ request, session }) => {
+  const res = await fetch(API_URL + '/subjects', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${await session?.get('accessToken')}`
+    },
+    body: JSON.stringify(await request.json())
+  })
+
+  return new Response(JSON.stringify(res))
+}
+
+export const GET: APIRoute = async ({ session }) => {
+  const res = await fetch(API_URL + `/subjects`, {
+    headers: {
+      Authorization: `Bearer ${await session?.get('accessToken')}`
+    }
+  }).then(res => {
+    return res.json()
+  })
+
+  return new Response(JSON.stringify(res))
+}
